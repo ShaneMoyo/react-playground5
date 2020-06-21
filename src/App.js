8c747f32f62678cdfe370d8542bc9f33699dfa62
shanemoyo <smoyo@pdx.edu>
2020-06-21 13:24:31 -0700

    add react routing.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import './App.css';
 import Increment from './components/Increment'; 
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route, 
+  Link 
+ } from 'react-router-dom'
 
 function App() {
+  const Home = () => <section><h1>Welcome to Home</h1><div>Have a look around</div></section>
   return (
     <div className="App">
-      <Increment/>
+      <Router>
+        <nav>
+          <ul> 
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/increment">Increment</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/increment" component={Increment}/>
+          <Route path="/" component={Home}/>
+        </Switch>
+      </Router> 
     </div>
   );
 }
